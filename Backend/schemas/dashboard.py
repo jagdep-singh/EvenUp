df08d1eb12a4bf46eecca2a5b60310cadcb43713
Krishna Gupta <kg640620@gmail.com>
2026-06-11 14:44:08 +0530

    Ruff check

--- Backend/schemas/dashboard.py
+++ Backend/schemas/dashboard.py
@@ -4,10 +4,12 @@ from schemas.expense_split import ExpenseResponse
 from schemas.groups import GroupResponse
 from schemas.user import UserResponse
 
+
 class GroupDashBoardResponse(BaseModel):
-      group: GroupResponse
-      expense: list[ExpenseResponse]
+    group: GroupResponse
+    expense: list[ExpenseResponse]
+
 
 class DashBoard(BaseModel):
-      user: UserResponse
-      groups: list[GroupDashBoardResponse]
+    user: UserResponse
+    groups: list[GroupDashBoardResponse]
